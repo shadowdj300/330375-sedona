5bda462b7a38b3db0f20e8dc24baf889a227efc5
shadowdj300 <shadowdj@yandex.ru>
2017-05-12 21:42:15 +0300

    Sedona
    Завершение проекта

---
+++ js/popup.js
@@ -0,0 +1,33 @@
+var link = document.querySelector(".hotel-search-btn-text");
+var popup = document.querySelector(".hotel-search");
+var form = document.querySelector(".hotel-search-form");
+var datein = form.querySelector("[name=date-in]");
+var dateout = form.querySelector("[name=date-out]");
+var adults = form.querySelector("[name=adults]");
+var children = form.querySelector("[name=children]");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.toggle("hotel-search-show");
+  datein.focus();
+});
+
+form.addEventListener("submit", function(evt) {
+  if (!adults.value || !children.value) {
+    evt.preventDefault();
+    console.log("Нужно выбрать количество людей");
+  }
+  else{
+    localStorage.setItem("adults", adults.value);
+    localStorage.setItem("children", children.value);
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+  if (popup.classList.contains("hotel-search-show")) {
+    popup.classList.remove("hotel-search-show");
+    popup.classList.remove("modal-error");
+    }
+  }
+ });
